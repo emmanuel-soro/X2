083c48944e5154409a8b0489c39371d1e1905388
kaiser-geek <emmanuel.soro@gmail.com>
2019-10-06 20:50:18 -0300

    Camara API relocacion

---
+++ Camara API/camera-api/src/main/java/edu/unlam/soa/x2/api/CameraApiApplication.java
@@ -0,0 +1,13 @@
+package edu.unlam.soa.x2.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CameraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CameraApiApplication.class, args);
+	}
+
+}
