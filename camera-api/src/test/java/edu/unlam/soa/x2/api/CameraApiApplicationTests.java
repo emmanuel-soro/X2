fe4896f571536c85c9a10581394c7879a0c85be9
kaiser-geek <emmanuel-soro@users.noreply.github.com>
2019-10-06 20:29:17 -0300

    Camera API Code

---
+++ camera-api/src/test/java/edu/unlam/soa/x2/api/CameraApiApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.unlam.soa.x2.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CameraApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
