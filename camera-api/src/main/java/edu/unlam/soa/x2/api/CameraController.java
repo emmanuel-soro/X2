a2d7181d130875ddd0ebc5b7ea6673e8ccef9eb5
kaiser-geek <emmanuel.soro@gmail.com>
2019-11-02 15:15:03 -0300

    Terminada version 2.1

--- camera-api/src/main/java/edu/unlam/soa/x2/api/CameraController.java
+++ camera-api/src/main/java/edu/unlam/soa/x2/api/CameraController.java
@@ -2,31 +2,41 @@ package edu.unlam.soa.x2.api;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/photo")
 public class CameraController {
 
-	@GetMapping
+	@GetMapping("/photo")
 	public ResponseEntity<String> takePhoto() {
 
-		String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+		String photoName = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
 
-		File file = new File("src/main/resources/ffmpeg/bin/ffmpeg.exe");
+		File file = new File("./ffmpeg/bin/ffmpeg.exe");
 		String absolutePath = file.getAbsolutePath();
 
-		System.out.println("Absoluth path: " + absolutePath);
+		File photoPath = new File("./photos");
+		String photosAbsolutePath = photoPath.getAbsolutePath();
 
 		String[] cmd = { absolutePath, "-y", "-i", "rtsp://admin:admin@192.168.1.195:554/h264/ch1/main/av_stream",
-				"-vframes", "1", time + ".jpg" };
+				"-vframes", "1", photosAbsolutePath + "\\" + photoName + ".jpg" };
+
+		System.out.println("Foto tomada con nombre " + photoName + ".jpg");
+
 		try {
 			Runtime.getRuntime().exec(cmd);
 		} catch (IOException e) {
@@ -34,7 +44,21 @@ public class CameraController {
 			e.printStackTrace();
 		}
 
-		return new ResponseEntity<String>("Works!!!", HttpStatus.OK);
+		return new ResponseEntity<String>(photoName, HttpStatus.OK);
+	}
+
+	@GetMapping(value = "/getPhoto", produces = MediaType.IMAGE_JPEG_VALUE)
+	public @ResponseBody byte[] getPhoto(@RequestParam String id) throws IOException {
+
+		File photo = new File("./photos/" + id + ".jpg");
+
+		return Files.readAllBytes(Paths.get(photo.getAbsolutePath()));
+
+	}
+
+	public Set<String> listFilesUsingJavaIO(String dir) {
+		return Stream.of(new File(dir).listFiles()).filter(file -> !file.isDirectory()).map(File::getAbsolutePath)
+				.collect(Collectors.toSet());
 	}
 
 }
