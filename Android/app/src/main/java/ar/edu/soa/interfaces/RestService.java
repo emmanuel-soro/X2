7e420f5abd9140dbc6ca0f7331d660de11c23057
kaiser-geek <emmanuel.soro@gmail.com>
2019-11-09 18:18:54 -0300

    Agrego activity tomar foto

--- Android/app/src/main/java/ar/edu/soa/interfaces/RestService.java
+++ Android/app/src/main/java/ar/edu/soa/interfaces/RestService.java
@@ -1,4 +1,16 @@
 package ar.edu.soa.interfaces;
 
+import ar.edu.soa.model.Imagen;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface RestService {
+
+    @GET("/getPhoto")
+    Call<ResponseBody> getPhoto(@Query("id") String photoID);
+
+
 }
