77883d18778d8ad42f943f4767d63925bd20f93f
kaiser-geek <emmanuel.soro@gmail.com>
2019-11-16 14:30:39 -0300

    Se agrega procesamiento de imagen en la API

--- camera-api/src/main/java/edu/unlam/soa/x2/api/CameraController.java
+++ camera-api/src/main/java/edu/unlam/soa/x2/api/CameraController.java
@@ -1,11 +1,17 @@
 package edu.unlam.soa.x2.api;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -52,4 +58,47 @@ public class CameraController {
 		return Files.readAllBytes(Paths.get(photo.getAbsolutePath()));
 
 	}
+
+	@GetMapping("/processImage")
+	public ResponseEntity<String> processImage(@RequestParam String name) {
+
+		String result = null;
+		String in = null;
+
+		try {
+
+			File pythonProgram = new File("./image-processing/mainFail.py");
+
+			File photo = new File("./photos/" + name);
+
+			String[] cmd = { "python", pythonProgram.getAbsolutePath(), photo.getAbsolutePath() };
+
+			Process p = Runtime.getRuntime().exec(cmd);
+
+			BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+			// read the output from the command
+			System.out.println("Standard output of the command:\n");
+
+			while ((in = stdInput.readLine()) != null) {
+				result = in;
+				System.out.println(in);
+			}
+
+			File database = new File("./image-processing/results.txt");
+
+			Path path = Paths.get(database.getAbsolutePath());
+
+			Files.write(path, Arrays.asList(result), StandardCharsets.UTF_8,
+					Files.exists(path) ? StandardOpenOption.APPEND : StandardOpenOption.CREATE);
+
+		} catch (IOException e) {
+			System.out.println("Exception happened - here's what I know: ");
+			e.printStackTrace();
+			return new ResponseEntity<String>(result, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		return new ResponseEntity<String>(result, HttpStatus.OK);
+	}
+
 }
