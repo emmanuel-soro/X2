3f02a93ad2391613a70d135b2759cf2c38924455
Gonzalo Rodeiro <chalorodeiro@gmail.com>
2019-11-16 17:56:58 -0300

    Adjunto idea de EstadoSensores

---
+++ Android/app/src/main/java/ar/edu/soa/smartfarm/EstadoSensoresActivity.java
@@ -0,0 +1,16 @@
+package ar.edu.soa.smartfarm;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EstadoSensoresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nosotros);
+    }
+}
