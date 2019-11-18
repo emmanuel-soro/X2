cf7a4430ca20e5c8b9e343cae5b7e6259fabc469
Gonzalo Rodeiro <chalorodeiro@gmail.com>
2019-11-18 14:40:16 -0300

    Subo la comunicacion con el arduino

--- Android/app/src/main/java/ar/edu/soa/interfaces/RestService.java
+++ Android/app/src/main/java/ar/edu/soa/interfaces/RestService.java
@@ -5,13 +5,16 @@ import ar.edu.soa.model.Imagen;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface RestService {
 
-
     @GET("/getPhoto")
     Call<ResponseBody> getPhoto(@Query("id") String photoID);
+
+    @GET("/dato")
+    Call<ResponseBody> getDato(@Query("id") String value);
 
 
 }
--- Android/app/src/main/java/ar/edu/soa/smartfarm/EstadoSensoresActivity.java
+++ Android/app/src/main/java/ar/edu/soa/smartfarm/EstadoSensoresActivity.java
@@ -1,16 +1,163 @@
 package ar.edu.soa.smartfarm;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import ar.edu.soa.interfaces.RestService;
+import okhttp3.OkHttpClient;
+import okhttp3.ResponseBody;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
 public class EstadoSensoresActivity extends AppCompatActivity {
+
+    Switch switchSensor, switchFollaje, switchReposo, switchTallo;
+    String value = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estado_sensores);
+
+        switchSensor = (Switch) findViewById(R.id.switchSensores);
+        switchFollaje = (Switch) findViewById(R.id.switchFollaje);
+        switchReposo = (Switch) findViewById(R.id.switchReposo);
+        switchTallo = (Switch) findViewById(R.id.switchTallo);
+
+        switchSensor.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    switchSensor.setChecked(true);
+                    switchFollaje.setChecked(false);
+                    switchReposo.setChecked(false);
+                    switchTallo.setChecked(false);
+                    value = "W";
+                    enviarPeticion(value);
+                }
+            }
+        });
+
+        switchFollaje.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    switchSensor.setChecked(false);
+                    switchFollaje.setChecked(true);
+                    switchReposo.setChecked(false);
+                    switchTallo.setChecked(false);
+                    value = "F";
+                    enviarPeticion(value);
+                }
+            }
+        });
+
+        switchReposo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    switchSensor.setChecked(false);
+                    switchFollaje.setChecked(false);
+                    switchReposo.setChecked(true);
+                    switchTallo.setChecked(false);
+                    value = "R";
+                    enviarPeticion(value);
+                }
+            }
+        });
+
+        switchTallo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    switchSensor.setChecked(false);
+                    switchFollaje.setChecked(false);
+                    switchReposo.setChecked(false);
+                    switchTallo.setChecked(true);
+                    value = "T";
+                    enviarPeticion(value);
+                }
+            }
+        });
+    }
+
+    public void enviarPeticion(String value) {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.level(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.1.39")
+                .client(client)
+                .build();
+
+        RestService restService = retrofit.create(RestService.class);
+        Call<ResponseBody> call = restService.getDato(value);
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                Toast toast1 =
+                        Toast.makeText(getApplicationContext(),
+                                 "OK", Toast.LENGTH_SHORT);
+
+                toast1.show();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Toast toast2 =
+                        Toast.makeText(getApplicationContext(),
+                                "ERROR", Toast.LENGTH_SHORT);
+
+                toast2.show();
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
